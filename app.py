a154c8a6f80f2d9e65476a56b90381b186226aab
0x7FFFFF <w13657804853@163.com>
2021-10-17 20:10:12 +0800

    Change RequestValidationError response

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 from src.apis import users, marks, mark_lists, subscribe
@@ -18,6 +19,14 @@ async def exception_handler(request: Request, exc: BaseError):
     return JSONResponse(
         status_code=exc.code,
         content={"code": exc.code, "msg": exc.message},
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=412,
+        content={"code": 412, "msg": "缺少参数或参数不合法"},
     )
 
 
